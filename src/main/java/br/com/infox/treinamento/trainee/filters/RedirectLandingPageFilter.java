d5d97c19ca06a0929fecbd78e325efc6cbad4b36
Erik Liberal <erikliberal@infox.com.br>
2021-01-14 15:37:41 -0300

    Adiciona filtro de redirecionamento de página inicial configurado por web.xml

---
+++ src/main/java/br/com/infox/treinamento/trainee/filters/RedirectLandingPageFilter.java
@@ -0,0 +1,45 @@
+package br.com.infox.treinamento.trainee.filters;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.logging.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class RedirectLandingPageFilter implements Filter {
+
+	private static final Logger LOG = Logger.getLogger("br.com.infox.treinamento.trainee.filters");
+
+	private String initParameter;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		LOG.info("INICIANDO FILTRO RedirectLandingPageFilter COM OS PARÂMETROS");
+		for (String nomeParametro : Collections.list(filterConfig.getInitParameterNames())) {
+			LOG.info(String.format("    '%s' => %s", nomeParametro, filterConfig.getInitParameter(nomeParametro)));
+		}
+
+		this.initParameter = filterConfig.getInitParameter("landingPage");
+	}
+
+	@Override
+	public void destroy() {
+		LOG.info("ENCERRANDO FILTRO RedirectLandingPageFilter");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		LOG.info("Iniciando método doFilter do RedirectLandingPageFilter");
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.sendRedirect(initParameter);
+		LOG.info("Finalizando método doFilter do RedirectLandingPageFilter");
+	}
+
+}
