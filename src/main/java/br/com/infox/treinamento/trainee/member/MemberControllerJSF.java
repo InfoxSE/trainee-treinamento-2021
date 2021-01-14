b270e87bea9ef33d361a97e455a81b76c2f11428
Erik Liberal <erikliberal@infox.com.br>
2021-01-14 18:09:13 -0300

    Nova interface para o cadastro de membros

---
+++ src/main/java/br/com/infox/treinamento/trainee/member/MemberControllerJSF.java
@@ -0,0 +1,34 @@
+package br.com.infox.treinamento.trainee.member;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.infox.treinamento.trainee.model.Member;
+
+@ManagedBean
+@RequestScoped
+public class MemberControllerJSF {
+
+	private static final Logger LOG = Logger.getLogger("br.com.infox.treinamento.trainee.member");;
+
+	private Member membroNovo;
+
+	@PostConstruct
+	public void init() {
+		this.membroNovo = new Member();
+	}
+
+	public void registrar() {
+		LOG.info("NOME => "+this.membroNovo.getName());
+		LOG.info("EMAIL => "+this.membroNovo.getEmail());
+		LOG.info("TELEFONE => "+this.membroNovo.getPhoneNumber());
+	}
+
+	public Member getMembroNovo() {
+		return membroNovo;
+	}
+
+}
