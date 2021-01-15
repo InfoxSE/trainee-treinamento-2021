2172735abb07fbcdde784b94603196f793d2f7e8
Erik Liberal <erikliberal@infox.com.br>
2021-01-15 16:20:32 -0300

    Refatora view de membro para view de pessoa física com CPF inclusive

---
+++ src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisica.java
@@ -0,0 +1,78 @@
+package br.com.infox.treinamento.trainee.pessoafisica;
+
+import java.util.Date;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class PessoaFisica {
+
+	@NotNull
+    @Size(min = 11, max = 11)
+    @Pattern(regexp = "[0-9]+", message = "Must contain only numbers")
+    private String cpf;
+
+    @NotNull
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
+    private String name;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min = 10, max = 12)
+    @Digits(fraction = 0, integer = 12)
+    private String phoneNumber;
+
+    @NotNull
+    private Date birthDate;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+    public Date getBirthDate() {
+		return birthDate;
+	}
+
+    public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
