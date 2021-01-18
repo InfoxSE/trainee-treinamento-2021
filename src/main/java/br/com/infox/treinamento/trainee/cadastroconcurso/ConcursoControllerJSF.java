f72e8815d23247591d9c1f531005b0b6fe1a9c4d
Edton Lemos <edh.15@hotmail.com>
2021-01-18 11:16:39 -0300

    Adicionando a validação da Data Limite.

--- src/main/java/br/com/infox/treinamento/trainee/cadastroconcurso/ConcursoControllerJSF.java
+++ src/main/java/br/com/infox/treinamento/trainee/cadastroconcurso/ConcursoControllerJSF.java
@@ -2,13 +2,16 @@ package br.com.infox.treinamento.trainee.cadastroconcurso;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @ViewScoped
@@ -34,16 +37,37 @@ public class ConcursoControllerJSF implements Serializable{
 		if (concursos == null) {
 			this.concursos = new ArrayList<>(0);
 		}
+		
+		//Validando data limite igual ou inferior a disponibilizacao
+		int dataComparada = validarDatas();
+		if (dataComparada <= 0) {
+			FacesContext.getCurrentInstance().addMessage("data",
+					new FacesMessage("A data limite não pode ser IGUAL ou ANTERIOR a data de disponibilização"));
+			return;
+		}
+		
+		
 		getConcursos().add(getNovoConcurso());
 		this.novoConcurso = new Concurso();
 		LOG.info("CONCURSOS DA LISTA");
 		for (Concurso concurso : concursos) {
 			LOG.info("NOME => " + concurso.getNome());
 			LOG.info("DESCRIÇÃO => " + concurso.getDescricao());
-			LOG.info("DATA DE DISPONIBILIZACAO => " + concurso.getDescricao());
-			LOG.info("DATA LIMITE => " + concurso.getDataDisponibilizacao());
-			LOG.info("QUANTIDADE => " + concurso.getDataLimite());
+			LOG.info("DATA DE DISPONIBILIZACAO => " + concurso.getDataDisponibilizacao());
+			LOG.info("DATA LIMITE => " + concurso.getDataLimite());
+			LOG.info("QUANTIDADE => " + concurso.getQuantidade());
 		}
+	}
+
+	private Integer validarDatas() {
+		Date dataLimite = novoConcurso.getDataLimite().getTime();
+		Date dataDisponibilizacao = novoConcurso.getDataDisponibilizacao().getTime();
+		
+		//O método compareTo() retorna 0 se as datas forem iguais, 
+		//e um valor menor que 0 se a data for anterior a data do argumento
+		int dataComparada = dataLimite.compareTo(dataDisponibilizacao);
+		
+		return dataComparada;
 	}
 
 	public Concurso getNovoConcurso() {
