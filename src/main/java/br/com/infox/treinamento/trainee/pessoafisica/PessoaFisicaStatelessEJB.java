91271b3d55611a2daff9ba3861c98069f8e52f8a
Erik Liberal <erikliberal@infox.com.br>
2021-01-19 16:41:00 -0300

    Adição de métodos de ciclo de vida no stateless session bean

--- src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisicaStatelessEJB.java
+++ src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisicaStatelessEJB.java
@@ -3,6 +3,8 @@ package br.com.infox.treinamento.trainee.pessoafisica;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -14,6 +16,15 @@ public class PessoaFisicaStatelessEJB implements PessoaFisicaService {
 	private int quantidadeAcessos = 0;
 	@EJB
 	private PessoaFisicaSingletonEJB pessoaFisicaSingletonEJB;
+
+	@PostConstruct
+	public void init() {
+		LOG.info("PostConstruct "+getClass().getSimpleName());
+	}
+	@PreDestroy
+	public void destroy() {
+		LOG.info("PreDestroy "+getClass().getSimpleName());
+	}
 
 	public void registrar(PessoaFisica novaPessoa) {
 		this.quantidadeAcessos++;
