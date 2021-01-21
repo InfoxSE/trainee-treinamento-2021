8265a76e80b8b64b8c19909506fc22a4ff5b4e57
Erik Liberal <erikliberal@infox.com.br>
2021-01-21 16:02:35 -0300

    Convertendo objeto PessoaFisica em entidade JPA

--- src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisica.java
+++ src/main/java/br/com/infox/treinamento/trainee/pessoafisica/PessoaFisica.java
@@ -1,7 +1,15 @@
 package br.com.infox.treinamento.trainee.pessoafisica;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -10,30 +18,51 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
-public class PessoaFisica {
+@Entity
+@Table(name = "tb_pessoa_fisica", uniqueConstraints = @UniqueConstraint(name="uk_pessoa_fisica_01", columnNames = "cd_cpf"))
+public class PessoaFisica implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_pessoa_fisica")
+	private Long id;
 
 	@NotNull
     @Size(min = 11, max = 11)
     @Pattern(regexp = "[0-9]+", message = "Must contain only numbers")
+	@Column(name="cd_cpf")
     private String cpf;
 
     @NotNull
     @Size(min = 1, max = 25)
     @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
+    @Column(name="nm_pessoa_fisica")
     private String name;
 
     @NotNull
     @NotEmpty
     @Email
+    @Column(name="ds_email")
     private String email;
 
     @NotNull
     @Size(min = 10, max = 12)
     @Digits(fraction = 0, integer = 12)
+    @Column(name="ds_phone_number")
     private String phoneNumber;
 
     @NotNull
+    @Column(name="dt_birth_date")
     private Date birthDate;
+
+    public Long getId() {
+		return id;
+	}
+    public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getCpf() {
 		return cpf;
@@ -74,5 +103,38 @@ public class PessoaFisica {
     public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaFisica other = (PessoaFisica) obj;
+
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+
+		return true;
+	}
+
+
 
 }
